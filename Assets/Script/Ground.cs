40e97bd5f7e35ecd91be1dc29ae74dfb41785b65
vraj1412 <sq.vraj1410@gmail.com>
2025-01-10 11:01:43 +0530

    groundComplete

--- Assets/Script/Ground.cs
+++ Assets/Script/Ground.cs
@@ -10,31 +10,77 @@ public class Ground : MonoBehaviour
     public float GroundLentgh = 30;
     public int NumOfGround = 2;
     public Transform PlayerTransform;
+    public Transform Obstacal1;
+    public Transform Obstacal2;
     
+    public bool IsActive= false;
 
+    public GamePlayManagar Ref_GamePlayManagar;
 
     public void Start()
     {
-        
-        for (int i = 0; i < NumOfGround; i++)
-        {
-            SpwanGround(i);
-        }
+        Ref_GamePlayManagar = GamePlayManagar.Instance;
+        //for (int i = 0; i < NumOfGround; i++)
+        //{
+        //    SpwanGround(i);
+        //}
     }
     public void Update()
     {
-        if (PlayerTransform.position.z  > ZSpwan  - (NumOfGround * GroundLentgh))
-        {
-            SpwanGround(Random.Range(0, 2));
-        }
+        //if (PlayerTransform.position.z  > ZSpwan  - (NumOfGround * GroundLentgh))
+        //{
+        //    SpwanGround(Random.Range(0, 2));
+        //}
         
     }
 
     
     public void SpwanGround(int Index)
     {
-        Instantiate(GroundPrefabes[Index], transform.position * ZSpwan,transform.rotation);
+        Instantiate(GroundPrefabes[Index], transform.forward * ZSpwan,transform.rotation);
         ZSpwan += GroundLentgh;
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+       // Debug.Log(collision.gameObject.tag);
+        if (collision.gameObject.tag == "Player")
+        {
+            IsActive =true;
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (IsActive)
+        { 
+            IsActive=false;
+            transform.localPosition = Ref_GamePlayManagar.LastGroundPostion + new Vector3(0,-0.1F,100f);
+            Ref_GamePlayManagar.LastGroundPostion = transform.localPosition;
+
+
+
+            Obstacal1.localPosition = new Vector3(GetXPostion(), 1, Random.Range(-0.4f , 0.0f));
+            Obstacal2.localPosition = new Vector3(GetXPostion(), 1, Random.Range(0.0f, 0.5f));
+        }
+    }
+
+    public float GetXPostion()
+    {
+        int x = Random.Range(0, 3);
+        if (x == 0)
+        {
+            return -0.35f;
+        }
+        else if(x == 1)
+        {
+            return 0;
+        }
+        else
+        {    
+            return 0.35f;
+        }
+
     }
 
 }
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,23 +7,30 @@ public class Player : MonoBehaviour
 {
     public Rigidbody rb;
     public float Speed = 10.0f;
-    public float Diraction = 0.02f;
+    public float Diraction = 20;
 
     public void Update()
     {
         PlayerMovement();
-       
     }
-
+    public void FixedUpdate()
+    {
+        //PlayerMovement();
+        
+    }
     public void PlayerMovement()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             rb.AddForce(new Vector3(-Diraction, 0, 0));
         }
-        if (Input.GetKey(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.D))
         {
             rb.AddForce(new Vector3(Diraction, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.X))
+        {
+            rb.AddForce(new Vector3(0, 0, -11f));
         }
 
         rb.AddForce(new Vector3(0, 0, Speed));
